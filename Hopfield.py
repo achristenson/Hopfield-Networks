af0eff9409cefde321fb22116233f7ad0ab3d4d7
Alex Christenson <11860900+alch5436@users.noreply.github.com>
2018-04-24 19:28:56 -0400

    Adding energy function for hopfield network

--- Hopfield.py
+++ Hopfield.py
@@ -14,15 +14,26 @@ class Hopfield:
 		# Create a vector for neuron states
 		self.V = np.array()
 
+		# Store the energy of the network
+		self.E = 0
+
 	# Training function to add data to the weights
 	# Can take single set of training data as vector or multiple sets as matrix
 	def train(self, x):
 		temp_W = x.dot(x.transpose())	# Gets weights via matrix math from data input
 		np.fill_diagonal(temp_W,0)		# Removes self weights from calculation
+		self.update_Energy(x)			# Update network energy
 		self.W = self.W + temp_W		# Updates the hopfield networks weight matrix
 
+	# Calculate and update Hopfield Energy, only works with vector inputs
+	def update_Energy(self,x):
+		E = -1*np.dot(x.transpose(),np.dot(W,x))/2
+		self.E += E
+
+	# Function which takes in an estimate and returns an approximation from memory
 	def recover(self, x, iters):
 		for n in range(iters):
 			i = random.randint(len(x))			# Choose a random neuron to fire off
 			xp = x.transpose.dot(self.W[:,i])	# Use a dummy to get the product
 			x[i] = xp.sign()					# Modify the neuron and repeat
+		return x
