73eea7c8000e77e7d1260e9ebc10b57af4061ddb
Alex Christenson <11860900+alch5436@users.noreply.github.com>
2018-05-08 18:01:06 -0400

    Adding a test file

---
+++ test.py
@@ -0,0 +1,13 @@
+# Testing the Hopfield Network
+# Import the class
+from Hopfield.py import Hopfield
+
+# Create the network
+my_network = Hopfield()
+
+# Process Data Here
+# ~~~
+# ~~~
+
+# Use the network!
+# my_network.learn(Whatever you wanna learn! :)
