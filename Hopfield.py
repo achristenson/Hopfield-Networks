a510c54eeed6433d6fef16b6abe21ebe5e24c68f
Alex Christenson <11860900+alch5436@users.noreply.github.com>
2018-05-08 20:44:26 -0400

    Updating test file

--- Hopfield.py
+++ Hopfield.py
@@ -1,15 +1,15 @@
 # Code for a discrete Hopfield Network
 
 # Importing the required functions for the network
-import numpy as numpy    # Numpy package for vector math
+import numpy as np    # Numpy package for vector math
 import random 			 # Random package for asynchronous firing
 
 # For now we can define the Hopfield network as its own class
 class Hopfield:
 	# Initialize the network with the required parameters
-	def __init__(self):
+	def __init__(self,n):
 		# Create a matrix for storing the weights of neuron connections
-		self.W = np.matrix()
+		self.W = np.matrix(np.zeros(n,n))
 
 		# Create a vector for neuron states
 		self.V = np.array()
@@ -20,14 +20,14 @@ class Hopfield:
 	# Training function to add data to the weights
 	# Can take single set of training data as vector or multiple sets as matrix
 	def train(self, x):
-		temp_W = x.dot(x.transpose())	# Gets weights via matrix math from data input
+		temp_W = np.outer(x,x.transpose())	# Gets weights via matrix math from data input
 		np.fill_diagonal(temp_W,0)		# Removes self weights from calculation
 		self.update_Energy(x)			# Update network energy
 		self.W = self.W + temp_W		# Updates the hopfield networks weight matrix
 
 	# Calculate and update Hopfield Energy, only works with vector inputs
 	def update_Energy(self,x):
-		E = -1*np.dot(x.transpose(),np.dot(W,x))/2
+		E = -1*np.outer(x.transpose(),np.outer(self.W,x))/2
 		self.E += E
 
 	# Function which takes in an estimate and returns an approximation from memory
