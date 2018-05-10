3fa9aed2d58b56780834291197facc01995ac639
Alex Christenson <11860900+alch5436@users.noreply.github.com>
2018-05-09 20:46:04 -0400

    Adding shit

--- Hopfield.py
+++ Hopfield.py
@@ -8,30 +8,37 @@ import random 			 # Random package for asynchronous firing
 class Hopfield:
 	# Initialize the network with the required parameters
 	def __init__(self):
-		# Create a matrix for storing the weights of neuron connections
-		self.hasRun = False
-
-		# Initialize weight matrix
-		self.W = 0
-
-		# Store the energy of the network
-		self.E = 0
+		self.hasRun = False		# Create a matrix for storing the weights of neuron connections
+		self.W = 0				# Initialize weight matrix
+		self.E = 0				# Store the energy of the network
+		self.theta = 0.5		# Create a bias, theta
+		
 
 	# Training function to add data to the weights
-	# Can take single set of training data as vector or multiple sets as matrix
+	# Can take single set of training data
 	def train(self, x):
-		temp_W = np.outer(x,x.transpose())	# Gets weights via matrix math from data input
-		temp_W = temp_W - np.identity(len(x))			# Removes self weights from calculation
-		print(temp_W)
-		self.update_Energy(x)				# Update network energy
-		# Check if the weight matrix has been initialized
+		self.update_Weights(x)		# Update the weight matrix
+		self.update_Energy(x)		# Update network energy
+
+	# Updates the weight matrix
+	def update_Weights(self,x):
+		w = np.zeros([len(x),len(x)]) 		# Initializes weights as 0
+		# Iterates each of the elements in the future weight matrix, does computation and saves as dummy
+		for j in range(len(x)):
+			for i in range(j,len(x)):
+				if i==j:
+					w[i,j] = 0
+				else:
+					w[i,j] = x[i]*x[j]
+					w[j,i] = w[i,j]
+
+		# Determines if the weight matrix has already been created, if not initialize, else add to existing weights
 		if self.hasRun == False:
 			self.hasRun = True
-			self.W = temp_W					# Initializes the weight matrix
+			self.W = w
 		else:
-			self.W = self.W + temp_W		# Updates the hopfield networks weight matrix
+			self.W = self.W + w
 
-		print(self.W)
 
 	# Calculate and update Hopfield Energy, only works with vector inputs
 	def update_Energy(self,x):
--- test.py
+++ test.py
@@ -16,13 +16,15 @@ def mat2vec(a):
 my_network = Hopfield()
 
 # Process Data Here ~~~~~~~ Whatever you want
-I = -1*np.ones([17,17])
+I = np.zeros([17,17])
 L = I
 L[:,0] = 1.0
 L[16,:] = 1.0
 I[0,:] = 1.0
 I[16,:] = 1.0
 I[:,8] = 1.0
+print(L)
+print(I)
 H = I.transpose()
 
 # Use the network!
@@ -38,11 +40,11 @@ Iprime = mat2vec(Iprime)
 
 my_network.train(I)
 print('Done training I')
-#my_network.train(H)
-#print('Done Training H')
+my_network.train(H)
+print('Done Training H')
 my_network.train(L)
 print('Done Training L')
 
 for i in range(1):
-	Iprime = my_network.recover(Iprime, 1)
+	Iprime = my_network.recover(Iprime, 100)
 	print(Iprime.reshape((17,17)))
